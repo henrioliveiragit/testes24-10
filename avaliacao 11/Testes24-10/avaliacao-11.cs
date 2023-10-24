318a305f18a25990c6954d1f1eaaddb419753905
Henrique Oliveira <61119602+henrioliveiragit@users.noreply.github.com>
2023-10-24 13:30:56 -0300

    Add files via upload

---
+++ avaliacao 11/Testes24-10/avaliacao-11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Testes24_10
+{
+
+
+class Program11
+    {
+        static void Main(string[] args)
+        {
+            // Lê um arquivo texto contendo uma lista de números inteiros
+            string[] linhas = File.ReadAllLines("numeros.txt");
+
+            // Converte as linhas em números inteiros
+            int[] numeros = linhas.Select(int.Parse).ToArray();
+
+            // Calcula a média dos números da lista
+            double media = numeros.Average();
+
+            // Imprime a média dos números da lista
+            Console.WriteLine("A média dos números da lista é {0}", media);
+        }
+    }
+}
